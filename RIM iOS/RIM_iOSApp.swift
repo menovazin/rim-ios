553616bf7c0492e511270e1e795897d89c46a4e3
 Denis Berezin <d.berezin@live.ru>
2026-07-02 02:14:54 +0300

    Initial Commit

---
+++ RIM iOS/RIM_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  RIM_iOSApp.swift
+//  RIM iOS
+//
+//  Created by  Den on 02.07.2026.
+//
+
+import SwiftUI
+
+@main
+struct RIM_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
